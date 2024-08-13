47f4c570a613d90198b8925bbf0014751d16e2e5
Mike Harder <mharder@microsoft.com>
2024-08-13 06:13:12 +0000

    Add multiple log exporters

--- app.ts
+++ app.ts
@@ -1,21 +1,21 @@
-import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
+// Optional and only needed to see the internal diagnostic logging (during development)
+// import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
 import { logs, SeverityNumber } from "@opentelemetry/api-logs";
 import {
   LoggerProvider,
   ConsoleLogRecordExporter,
   SimpleLogRecordProcessor,
 } from "@opentelemetry/sdk-logs";
-import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-grpc';
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-grpc";
 
-// Optional and only needed to see the internal diagnostic logging (during development)
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-
-// const loggerExporter = new ConsoleLogRecordExporter();
-const loggerExporter = new OTLPLogExporter();
 const loggerProvider = new LoggerProvider();
-
 loggerProvider.addLogRecordProcessor(
-  new SimpleLogRecordProcessor(loggerExporter)
+  new SimpleLogRecordProcessor(new ConsoleLogRecordExporter())
+);
+loggerProvider.addLogRecordProcessor(
+  new SimpleLogRecordProcessor(new OTLPLogExporter())
 );
 
 logs.setGlobalLoggerProvider(loggerProvider);
