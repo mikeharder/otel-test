62f9291f9446c3c4996935ffb5293e433f66f27f
Mike Harder <mharder@microsoft.com>
2024-08-15 05:22:21 +0000

    Update helpers

--- utils.ts
+++ utils.ts
@@ -1,11 +1,11 @@
-import { Span, SpanStatusCode, Tracer } from "@opentelemetry/api";
+import { Span, SpanOptions, SpanStatusCode, Tracer } from "@opentelemetry/api";
 
 export async function withSpan<T>(
   tracer: Tracer,
   name: string,
   fn: (span: Span) => Promise<T>,
 ): Promise<T> {
-  return tracer.startActiveSpan(name, async (span: Span) => {
+  return await tracer.startActiveSpan(name, async (span: Span) => {
     try {
       let result = await fn(span);
       span.setStatus({ code: SpanStatusCode.OK });
@@ -20,12 +20,45 @@ export async function withSpan<T>(
   });
 }
 
-export function withSpanSync<T>(
+export async function withSpanOptions<T>(
   tracer: Tracer,
   name: string,
-  fn: (span: Span) => T,
-): T {
+  options: SpanOptions,
+  fn: (span: Span) => Promise<T>,
+): Promise<T> {
+  return await tracer.startActiveSpan(name, options, async (span: Span) => {
+    try {
+      let result = await fn(span);
+      span.setStatus({ code: SpanStatusCode.OK });
+      return result;
+    } catch (error) {
+      span.recordException(error as any);
+      span.setStatus({ code: SpanStatusCode.ERROR });
+      throw error;
+    } finally {
+      span.end();
+    }
+  });
+}
+
+export function withSpanSync<T>(tracer: Tracer, name: string, fn: (span: Span) => T): T {
   return tracer.startActiveSpan(name, (span: Span) => {
+    try {
+      let result = fn(span);
+      span.setStatus({ code: SpanStatusCode.OK });
+      return result;
+    } catch (error) {
+      span.recordException(error as any);
+      span.setStatus({ code: SpanStatusCode.ERROR });
+      throw error;
+    } finally {
+      span.end();
+    }
+  });
+}
+
+export function withSpanOptionsSync<T>(tracer: Tracer, name: string, options: SpanOptions, fn: (span: Span) => T): T {
+  return tracer.startActiveSpan(name, options, (span: Span) => {
     try {
       let result = fn(span);
       span.setStatus({ code: SpanStatusCode.OK });
